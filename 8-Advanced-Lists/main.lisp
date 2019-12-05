3b9e78902b39254520e3d823c462e643ce089525
Oleh SAMOILENKO <oilsamoilenko@gmail.com>
2019-12-05 10:07:34 +0200

    LR8 + report done.
    Location: Restoran

--- 8-Advanced-Lists/main.lisp
+++ 8-Advanced-Lists/main.lisp
@@ -2,8 +2,26 @@
 ;; Якщо списки не однакові, виконати їх конкатенацію, інакше утворити новий список,
 ;; значення елементів яких є парними або непарними числами.
 
-(setq lst1 '("hello" "hello2"))
-(setq lst2 '("hello" "hello"))
+;; return:
+;; list of elements' lengths
+(defun form-list-of-lengths (numbers words)
+	(if (equalp NIL (cdr words))
+		(list (length (car words)))
+		;; else
+		(append
+			(list (length (car words)))
+			(form-list-of-lengths numbers (cdr words))
+		)
+	)
+)
 
-(print (equalp lst1 lst2))
-(print (append lst1 lst2))
+(defun process (lst1 lst2)
+	(if (equalp lst1 lst2)
+		(form-list-of-lengths '() lst1)
+		;; else
+		(append lst1 lst2)
+	)
+)
+
+(print (process '("1" "22") '("333" "4444")))
+(print (process '("hi" "i" "am" "oleh") '("hi" "i" "am" "oleh")))
